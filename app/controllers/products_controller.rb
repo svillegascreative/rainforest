ca323059589fc5832d406e52c3f013d1b8d23738
sylvia villegas <sylvia@svillegas.com>
2017-02-06 15:37:37 -0500

    Add edit/update actions, view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,20 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update_attirbutes(product_params)
+      redirect_to product_url(@product)
+    else
+      render :edit
+    end
+  end
+
 private
   def product_params
     params.require(:product).permit(:name, :description, :price_in_cents)
