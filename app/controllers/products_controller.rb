f4ec5ebe5535c75644c6006ab2d63df9a6e90ef5
sylvia villegas <sylvia@svillegas.com>
2017-02-06 15:31:03 -0500

    Add new/create actions, view, & form partial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,23 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_url
+    else
+      render :new
+    end
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
+
 end
