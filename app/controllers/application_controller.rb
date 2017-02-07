cd918026f737fc5b5c9375cbc93146d2570159b1
sylvia villegas <sylvia@svillegas.com>
2017-02-06 22:51:35 -0500

    Add authorization for users to post reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
 private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+  def ensure_logged_in
+    unless current_user
+      redirect_to new_session_url, alert: "Please log in"
+    end
+  end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
 
   before_action :load_product
+  before_action :ensure_logged_in, only: [:create, :destroy]
 
   def show
     @review = Review.find(params[:id])
